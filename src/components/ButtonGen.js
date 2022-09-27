9df8c78cccfaa67f7c762a9733e093c521b8eab9
Dosya <96661032+VicDosya@users.noreply.github.com>
2022-09-27 14:17:34 +0300

    Favorite word2 (#3)
    * Add favorite button
    
    * Extract favorite words into a new component
    
    * Format document
    
    * Remove useless code

--- src/components/ButtonGen.js
+++ src/components/ButtonGen.js
@@ -1,25 +1,38 @@
 import { React, useState } from 'react'
 import styles from './ButtonGen.module.css';
 import randomWords from 'random-words';
+import Badges from './Badges';
 
 function ButtonGen() {
 
     //State variables
-    const [valueName, setValueName] = useState('');
-
-    //Random string picker from the namesArray
-    const randomNamePicker = () => {
-        const pickedName = randomWords();
-        return pickedName;
-    };
+    const [generatedWord, setGeneratedWord] = useState('');
+    const [error, setError] = useState('');
+    const [favWords, setFavWords] = useState([]);
 
     //onClick function to print the pickedName from the randomNamePicker function.
-    const generateRandomName = () => {
-        setValueName(randomNamePicker);
+    const generateRandomWord = () => {
+        setGeneratedWord(randomWords());
+        setError('');
+    };
+
+    //onClick function to save favorite words.
+    const favButtonHandler = () => {
+        if (generatedWord === '' || generatedWord === 'Cant save empty') {
+            setError('Cant save empty');
+        } else if (favWords.includes(generatedWord)) {
+            setError('Cant save duplicates');
+        } else if (favWords.length > 20) {
+            setError('Cant fav no more');
+        } else {
+            setFavWords([...favWords, generatedWord]);
+            setError('');
+        }
     };
 
     //JSX
     return (
+
         <div className={styles.mainContainer}>
             <div className={styles.titleContainer}>
                 <h1 className={styles.mainTitle}>Random Word Generator</h1>
@@ -29,14 +42,21 @@ function ButtonGen() {
                 <p className={styles.mainDescription}>Press the "Generate" button to generate a random word.</p>
             </div>
 
-            <div className={styles.randomNameContainer}>
-                <div className={styles.randomNameText}>{valueName}</div>
+            <div className={styles.errorContainer}>
+                <div className={styles.errorText}>{error}</div>
+            </div>
+
+            <div className={styles.randomWordContainer}>
+                <div className={styles.randomWordText}>{generatedWord}
+                    <button className={styles.heartButton} onClick={favButtonHandler}>❤</button>
+                </div>
             </div>
 
             <div className={styles.buttonContainer}>
-                <button className={styles.generateButton} onClick={generateRandomName}>Generate</button>
+                <button className={styles.generateButton} onClick={generateRandomWord}>Generate</button>
             </div>
 
+            <Badges values={favWords}></Badges>
         </div>
     )
 };
