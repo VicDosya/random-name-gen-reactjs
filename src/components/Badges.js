60ca0be9b5a685a3cc5bfb055a7984d9fd632ee0
dosya <vicdosya@gmail.com>
2022-09-27 14:06:05 +0300

    Format document

--- src/components/Badges.js
+++ src/components/Badges.js
@@ -1,12 +1,11 @@
 import React from 'react'
 import styles from './Badges.module.css'
 
-function Badges({values}) {
+function Badges({ values }) {
     return (
-            <div className={styles.container}>{values.map((value, index) =>
-                <div className={styles.badge} key={index}>{value}</div>
-            )}</div>
-        
+        <div className={styles.container}>{values.map((value, index) =>
+            <div className={styles.badge} key={index}>{value}</div>
+        )}</div>
     )
 }
 
